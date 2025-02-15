e25f235c7b0d784801635ea61a620f1c06a4f6e4
unknown <bejisouhir2@gmail.Com>
2025-02-15 21:28:08 +0100

    aaaa

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",  // Définit la base de l'application
-  esbuild: {
-    jsxInject: `import React from 'react'`,  // Évite les erreurs avec JSX
-  }
+  
 })
